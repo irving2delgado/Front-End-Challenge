76460c394771e8b201016a98f282791b60a94dfa
irving2delgado <36970739+irving2delgado@users.noreply.github.com>
2018-05-30 16:23:28 -0600

    added switching events page

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 var card = $('#mix');
 var mixList = $.makeArray(card) ;
 
-var return_first;
-function callback(response) {
-  return_first = response;
-  //use return_first variable here
-  $("#")
-  console.log(return_first);
-}
-
-
 $(window).on('load', function(){
 	$.getJSON('http://www.mocky.io/v2/5b0ec03d3200004c00c19c23')
 	.done(function(data){
@@ -21,8 +12,8 @@ $(window).on('load', function(){
 		city = mix[0].city;
 
 
-		$('#mix').append(city);
-		$("#mixImg").css("background-image", "url(" + url + ")");
+		$('#mixCity').append(city);
+		$("#card2").css("background-image", "url(" + url + ")");
 
 		callback(data);
 	})
@@ -31,6 +22,36 @@ $(window).on('load', function(){
   	})
 
 });
+
+// $(" #toggleCard2").click(function(){
+//   	$('.destinations').switchClass('destinations','hideCard');
+//   	$('.').addClass('.hideCard');
+//   });
+function show(shown, hidden) {
+  document.getElementById(shown).style.display='block';
+  document.getElementById(hidden).style.display='none';
+  return false;
+}
+
+var return_first;
+function callback(response) {
+  return_first = response;
+  //use return_first variable here
+  let $detail2 = $("#.detail2");
+  let $card2 = $(".card2")
+
+  // $("#toggleCard").click(function(){
+  // if ($detail2.is(':hidden')){
+  // 	$card2.hide();
+
+  // }
+	// $("li #toggleCard2").click(function(){
+ //  	$('section > .singleEvent').removeClass('.hideCard');
+ //  	$('section > .destinations').addClass('.hideCard');
+ //  });
+  console.log(return_first);
+}
+
 
 
 
