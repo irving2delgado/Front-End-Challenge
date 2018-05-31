882bc7c9c1ed5d405b4daea30f3018e25100d2c6
irving2delgado <36970739+irving2delgado@users.noreply.github.com>
2018-05-30 22:58:17 -0600

    improving functionality

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
- var map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-        ],
-        view: new ol.View({
-          center: ol.proj.fromLonLat([37.41, 8.82]),
-          zoom: 4
-        })
-      });
+ function show(shown, hidden) {
+  document.getElementById(shown).style.display='block';
+  document.getElementById(hidden).style.display='none';
+  return false;
+}
 
 
 $(window).on('load', function(){
-	$.getJSON('http://www.mocky.io/v2/5b0f67de3000006400115058')
+	$.getJSON('http://www.mocky.io/v2/5b0f7db53000000e001150a5')
 	.done(function(data){
 		console.log(data);
 				$( ".destinations li" ).each(function( i ) {
@@ -25,10 +18,10 @@ $(window).on('load', function(){
   			var mix = data;
 		url = mix[i].image;
 		city = mix[i].city;
-		description = mix[0].description;
-		duration = mix[0].duration;
-		time = mix[0].time;
-		date = mix[0].date;
+		description = mix[i].description;
+		duration = mix[i].duration;
+		time = mix[i].time;
+		date = mix[i].date;
 		var eventSelect = ('"#toggleCard' + i + '"');
 		$(this).css("background-image", "url(" + url + ")");
 });
@@ -51,7 +44,7 @@ $(window).on('load', function(){
 		callback(data);
 	})
 		
-		$(this).click(function(data){
+		$(this).click(function(mix){
   	$('.singleEvent > .grid > li > h1').append(city);
   	$('.singleEvent > .grid > li > #description').append(description);
   	$('.singleEvent > .grid > li > #time').append(time);
@@ -59,18 +52,18 @@ $(window).on('load', function(){
   	$('#singleImg').css("background-image", "url(" + url + ")");
   })
 	
-	 var map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-        ],
-        view: new ol.View({
-          center: ol.proj.fromLonLat([37.41, 8.82]),
-          zoom: 4
-        })
-      });
+	 // var map = new ol.Map({
+  //       target: 'map',
+  //       layers: [
+  //         new ol.layer.Tile({
+  //           source: new ol.source.OSM()
+  //         })
+  //       ],
+  //       view: new ol.View({
+  //         center: ol.proj.fromLonLat([37.41, 8.82]),
+  //         zoom: 4
+  //       })
+  //     });
 
 	// .fail(function(){
  //  			//fail code here
