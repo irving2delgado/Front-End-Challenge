d9ce7eef299b4d014fdf9a522de3fc106349a941
irving2delgado <36970739+irving2delgado@users.noreply.github.com>
2018-05-31 12:35:29 -0600

    functionality finalized

--- index.js
+++ index.js
@@ -1,121 +1,267 @@
+//  ******************* Pages toggle button
 
 $("#toggleBack").click(function(){
 			$(".singleEvent > ul > li > h1").empty();
 			$(".singleEvent > ul > li > p").empty();
 	
-	})
-	function show(shown, hidden) {
+});
+
+function show(shown, hidden) {
   			document.getElementById(shown).style.display='block';
   			document.getElementById(hidden).style.display='none';
   			return false;
-		}	
+};	
 
+// *************** Load JSON Data 
 
 $(window).on('load', function(){
-	$.getJSON('http://www.mocky.io/v2/5b0ffc473000006a0011535d')
-	.done(function(data){
-		console.log(data);
-				$( ".destinations li" ).each(function( i ) {
-			// var i = index;
-  console.log( i + ": " + $( this ).text() );
-  console.log(i);
-  	// $('"#toggleCard' + i + '"').css("background-image", "url(" + url + ")");
-  		// $('"#toggleCard' + i + '"').css("background-image", "url(" + url + ")");
+	$.getJSON('http://www.mocky.io/v2/5b10088930000010001153a2')
+		.done(function(data){
+			console.log(data);
+
+		$( ".destinations li" ).each(function( i ) {			
+  			console.log( i + ": " + $( this ).text() );
+  			console.log(i);
+  	
   			var mix = data;
-		url = mix[i].image;
-		city = mix[i].city;
-		description = mix[i].description;
-		duration = mix[i].duration;
-		time = mix[i].time;
-		date = mix[i].date;
-		// var eventSelect = ('"#toggleCard' + i + '"');
-		$(this).append(city);
-		$(this).css("background-image", "url(" + url + ")");
+			url = mix[i].image;
+			city = mix[i].city;
+			description = mix[i].description;
+			duration = mix[i].duration;
+			time = mix[i].time;
+			date = mix[i].date;
+			$(this).append(city);
+			$(this).css("background-image", "url(" + url + ")");
 
-			$(this).click(function(){
-  	$('.singleEvent > .grid > li > h1').append(city);
-  	$('.singleEvent > .grid > li > #description').append(description);
-  	$('.singleEvent > .grid > li > #time').append(time);
-  	$('.singleEvent > .grid > li > #duration').append(duration + "Hrs");
-  	$('.singleEvent > .grid > li > #singleImg').css("background-image", "url(" + url + ")");
-  })
-
-});
-		// var mix = data;
-		// url = mix[i].image;
-		// city = mix[0].city;
-		// description = mix[0].description;
-		// duration = mix[0].duration;
-		// time = mix[0].time;
-		// date = mix[0].date;
-		// var eventSelect = ('"#toggleCard' + i + '"');
-
-	
-
-
-
-		$('#mixCity').append(city);
-		// $('"#toggleCard' + i + '"').css("background-image", "url(" + url + ")");
+		});
 
 		callback(data);
-	})
-		
-		// $(this).click(function(mix){
-  // 	$('.singleEvent > .grid > li > h1').append(city);
-  // 	$('.singleEvent > .grid > li > #description').append(description);
-  // 	$('.singleEvent > .grid > li > #time').append(time);
-  // 	$('.singleEvent > .grid > li > #duration').append(duration + "Hrs");
-  // 	$('.singleEvent > .grid > li > #singleImg').css("background-image", "url(" + url + ")");
-  // })
+		// I used this function to acces data from outside this Ajax request
+		});
 	
-	// $("#toggleBack").click(function(){
-	// 		$(".singleEvent > .grid > li").removeData();
-	// 		$(".singleEvent > .grid > li > p").removeData();
-	// 	function show(shown, hidden) {
- //  			document.getElementById(shown).style.display='block';
- //  			document.getElementById(hidden).style.display='none';
- //  			return false;
-	// 	}	
-	// })
+		// .fail(function(){
+ 		//  			//fail code here
+ 		//  	})
 
-	
-	// .fail(function(){
- //  			//fail code here
- //  	})
+}); 
 
-});
-$("#toggleBack").click(function(){
-			$(".singleEvent > .grid > li").removeData();
-			$(".singleEvent > .grid > li > p").removeData();
-		function show(shown, hidden) {
-  			document.getElementById(shown).style.display='block';
-  			document.getElementById(hidden).style.display='none';
-  			return false;
-		}	
-	})
+var mix;
 
-
-	 var map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          })
-        ],
-        view: new ol.View({
-          center: ol.proj.fromLonLat([37.41, 8.82]),
-          zoom: 4
-        })
-      });
-
-
-var return_first;
 function callback(response) {
-  return_first = response;
+  mix = response;
+  i =0;
   
-  }
-	
-  console.log(return_first);
+// ************************Mix Card 1 ****************************
+
+  	$("#toggleCard1").click(function(){
+  		i = 0;
+  		$('.singleEvent > .grid > li > h1').append(mix[i].city);
+  		$('.singleEvent > .grid > li > #description').append(mix[i].description);
+  		$('.singleEvent > .grid > li > #time').append(mix[i].time);
+  		$('.singleEvent > .grid > li > #duration').append(mix[i].duration + "Hrs");
+  		$('#singleImg').css("background-image", "url(" + mix[i].image + ")");
+  		x = mix[i].location_lng;
+  		y = mix[i].location_lat;
+
+  		var map = new ol.Map({
+        	target: 'map',
+        	layers: [
+          	new ol.layer.Tile({
+            	source: new ol.source.OSM()
+          	})
+        	],
+       	 	view: new ol.View({
+          	center: ol.proj.fromLonLat([x , y]),
+          	zoom: 12
+        	})
+     	});
+	});
+
+	// ************************Mix Card 2 ****************************
+
+
+	$("#toggleCard2").click(function(){
+  		i = 1;
+  		$('.singleEvent > .grid > li > h1').append(mix[i].city);
+  		$('.singleEvent > .grid > li > #description').append(mix[i].description);
+  		$('.singleEvent > .grid > li > #time').append(mix[i].time);
+  		$('.singleEvent > .grid > li > #duration').append(mix[i].duration + "Hrs");
+  		$('#singleImg').css("background-image", "url(" + mix[i].image + ")");
+  		x = mix[i].location_lng;
+  		y = mix[i].location_lat;
+
+  		var map = new ol.Map({
+        	target: 'map',
+        	layers: [
+          	new ol.layer.Tile({
+            	source: new ol.source.OSM()
+          	})
+        	],
+       	 	view: new ol.View({
+          	center: ol.proj.fromLonLat([x , y]),
+          	zoom: 12
+        	})
+     	});
+	});	
+
+	// ************************Mix Card 3 ****************************
+
+	$("#toggleCard3").click(function(){
+  		i = 2;
+  		$('.singleEvent > .grid > li > h1').append(mix[i].city);
+  		$('.singleEvent > .grid > li > #description').append(mix[i].description);
+  		$('.singleEvent > .grid > li > #time').append(mix[i].time);
+  		$('.singleEvent > .grid > li > #duration').append(mix[i].duration + "Hrs");
+  		$('#singleImg').css("background-image", "url(" + mix[i].image + ")");
+  		x = mix[i].location_lng;
+  		y = mix[i].location_lat;
+
+  		var map = new ol.Map({
+        	target: 'map',
+        	layers: [
+          	new ol.layer.Tile({
+            	source: new ol.source.OSM()
+          	})
+        	],
+       	 	view: new ol.View({
+          	center: ol.proj.fromLonLat([x , y]),
+          	zoom: 12
+        	})
+     	});
+	});	
+
+	// ************************Mix Card 4 ****************************
+
+	$("#toggleCard4").click(function(){
+  		i = 3;
+  		$('.singleEvent > .grid > li > h1').append(mix[i].city);
+  		$('.singleEvent > .grid > li > #description').append(mix[i].description);
+  		$('.singleEvent > .grid > li > #time').append(mix[i].time);
+  		$('.singleEvent > .grid > li > #duration').append(mix[i].duration + "Hrs");
+  		$('#singleImg').css("background-image", "url(" + mix[i].image + ")");
+  		x = mix[i].location_lng;
+  		y = mix[i].location_lat;
+
+  		var map = new ol.Map({
+        	target: 'map',
+        	layers: [
+          	new ol.layer.Tile({
+            	source: new ol.source.OSM()
+          	})
+        	],
+       	 	view: new ol.View({
+          	center: ol.proj.fromLonLat([x , y]),
+          	zoom: 12
+        	})
+     	});
+	});	
+
+	// ************************Mix Card 5 ****************************
+
+	$("#toggleCard5").click(function(){
+  		i = 4;
+  		$('.singleEvent > .grid > li > h1').append(mix[i].city);
+  		$('.singleEvent > .grid > li > #description').append(mix[i].description);
+  		$('.singleEvent > .grid > li > #time').append(mix[i].time);
+  		$('.singleEvent > .grid > li > #duration').append(mix[i].duration + "Hrs");
+  		$('#singleImg').css("background-image", "url(" + mix[i].image + ")");
+  		x = mix[i].location_lng;
+  		y = mix[i].location_lat;
+
+  		var map = new ol.Map({
+        	target: 'map',
+        	layers: [
+          	new ol.layer.Tile({
+            	source: new ol.source.OSM()
+          	})
+        	],
+       	 	view: new ol.View({
+          	center: ol.proj.fromLonLat([x , y]),
+          	zoom: 12
+        	})
+     	});
+	});	
+
+	// ************************Mix Card 6 ****************************
+
+	$("#toggleCard6").click(function(){
+  		i = 5;
+  		$('.singleEvent > .grid > li > h1').append(mix[i].city);
+  		$('.singleEvent > .grid > li > #description').append(mix[i].description);
+  		$('.singleEvent > .grid > li > #time').append(mix[i].time);
+  		$('.singleEvent > .grid > li > #duration').append(mix[i].duration + "Hrs");
+  		$('#singleImg').css("background-image", "url(" + mix[i].image + ")");
+  		x = mix[i].location_lng;
+  		y = mix[i].location_lat;
+
+  		var map = new ol.Map({
+        	target: 'map',
+        	layers: [
+          	new ol.layer.Tile({
+            	source: new ol.source.OSM()
+          	})
+        	],
+       	 	view: new ol.View({
+          	center: ol.proj.fromLonLat([x , y]),
+          	zoom: 12
+        	})
+     	});
+	});	
+
+	// ************************Mix Card 7 ****************************
+
+	$("#toggleCard7").click(function(){
+  		i = 6;
+  		$('.singleEvent > .grid > li > h1').append(mix[i].city);
+  		$('.singleEvent > .grid > li > #description').append(mix[i].description);
+  		$('.singleEvent > .grid > li > #time').append(mix[i].time);
+  		$('.singleEvent > .grid > li > #duration').append(mix[i].duration + "Hrs");
+  		$('#singleImg').css("background-image", "url(" + mix[i].image + ")");
+  		x = mix[i].location_lng;
+  		y = mix[i].location_lat;
+
+  		var map = new ol.Map({
+        	target: 'map',
+        	layers: [
+          	new ol.layer.Tile({
+            	source: new ol.source.OSM()
+          	})
+        	],
+       	 	view: new ol.View({
+          	center: ol.proj.fromLonLat([x , y]),
+          	zoom: 12
+        	})
+     	});
+	});
+
+	// ************************Mix Card 8 ****************************
+
+	$("#toggleCard8").click(function(){
+  		i = 7;
+  		$('.singleEvent > .grid > li > h1').append(mix[i].city);
+  		$('.singleEvent > .grid > li > #description').append(mix[i].description);
+  		$('.singleEvent > .grid > li > #time').append(mix[i].time);
+  		$('.singleEvent > .grid > li > #duration').append(mix[i].duration + "Hrs");
+  		$('#singleImg').css("background-image", "url(" + mix[i].image + ")");
+  		x = mix[i].location_lng;
+  		y = mix[i].location_lat;
+
+  		var map = new ol.Map({
+        	target: 'map',
+        	layers: [
+          	new ol.layer.Tile({
+            	source: new ol.source.OSM()
+          	})
+        	],
+       	 	view: new ol.View({
+          	center: ol.proj.fromLonLat([x , y]),
+          	zoom: 12
+        	})
+     	});
+	});
+
+};
+
 
 
 
